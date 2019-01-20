b4e519f471499b28afd73ec5f4c0d56d49ddbdf6
NatalieMatson <nataliequinndesign@gmail.com>
2019-01-20 18:22:26 -0500

    Setting up MongoDB and the Server.

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,28 @@
-// Importing dependencies.
+// Setting up dependencies.
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import { getSecret } from './secrets';
 
-// Setting up Instances.
+// Creating instances.
 const app = express();
 const router = express.Router();
 
+// Setting port to 3001.
 const API_PORT = process.env.API_PORT || 3001;
 
-// Configuring API to use BodyParser and look for JSON Data.
-app.use(bodyParser.urlencoded({ extended: false }));
+// Setting URL to mLab - found in a secrets.js unavailable publicly.
+mongoose.connect(getSecret('dbUri'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Configuring API to use bodyParser and look for JSON data.
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// Set route path, initialize API.
+// Setting route path and initializing API.
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
-
-// Use router configuration when API is called.
-app.use('/api', router);
-
-app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
